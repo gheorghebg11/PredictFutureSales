1932ddf642f7c711e2f0217d6292e9626d1a978f
Bogdan Gheorghe <gheorghebg@gmail.com>
2019-02-27 10:42:26 +0100

    boosting added

--- 3_XGBboost.py
+++ 3_XGBboost.py
@@ -31,26 +31,33 @@ df_train['item_cnt_day'] = df_train['item_cnt_day'].clip(0,100)
 df_train = df_train[baseline_features].groupby(['shop_id', 'item_id', 'date_block_num']).agg({'item_cnt_day':np.sum, 'item_category':np.mean}).reset_index() # I have to be careful here if item_cat are not all the same (although they SHOULD be, and they are)
 df_train.rename(columns={'item_cnt_day':'item_cnt_month'}, inplace=True)
 
+baseline_features.remove('item_cnt_day')
+baseline_features.append('item_cnt_month')
+
 # Remove pandas index column
 df_train['item_cnt_month'] = df_train.item_cnt_month.fillna(0).clip(0,100)
 
-
-print(df_test.head())
-
+df_trainx = df_train.drop('item_cnt_month', axis=1)
+df_trainy = df_train['item_cnt_month']
+print(df_trainx.columns)
 
 # Make test_dataset pandas data frame, add category id and date block num, then convert back to numpy array and predict
-merged_test['date_block_num'] = 33
-merged_test.set_index('shop_id')
-merged_test.head(3)
+df_test['date_block_num'] = 33
+print(df_test.head())
 
-model = xgb.XGBRegressor(max_depth = 10, min_child_weight=0.5, subsample = 1, eta = 0.3, num_round = 1000, seed = 1)
-model.fit(trainx, trainy, eval_metric='rmse')
-preds = model.predict(merged_test.values)
+print('Creating the model')
+model = xgb.XGBRegressor(max_depth =1, min_child_weight=0.5, subsample = 1, eta = 0.3, num_round = 10, seed = 1)
+print('Model created, now fitting.')
+model.fit(df_trainx.values, df_trainy.values, eval_metric='rmse')
+print('Model fit on data. Now predicting')
+preds = model.predict(df_test[df_trainx.columns].values)
+print('Prediction done.')
 
-df = pd.DataFrame(preds, columns = ['item_cnt_month'])
-df['ID'] = df.index
-df = df.set_index('ID')
-df.to_csv('simple_xgb.csv')
+#df = pd.DataFrame(preds, columns = ['item_cnt_month'])
+df_test['item_cnt_month'] = preds
+df_test[['ID', 'item_cnt_month']].to_csv('simple_xgb.csv')
+
+
 
 
 
